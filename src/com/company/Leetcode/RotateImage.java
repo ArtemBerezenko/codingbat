b5820977605078bd69a9329d686ca89e21ffed3a
ArtemBerezenko <a.a.berezenko@gmail.com>
2021-10-13 09:38:06 +0300

    leetcode::704: Binary Search

--- src/com/company/Leetcode/RotateImage.java
+++ src/com/company/Leetcode/RotateImage.java
@@ -9,7 +9,7 @@ public class RotateImage {
         int r2Temp;
         int n = matrix.length;
 
-        for (int i = 0; i < (n + 1) / 2; i ++) {
+        for (int i = 0; i < (n + 1) / 2; i++) {
             for (int j = 0; j < n / 2; j++) {
                 r1Temp = matrix[i][j];
                 c2Temp = matrix[j][n - 1 - i];
@@ -26,9 +26,9 @@ public class RotateImage {
 
     public static void main(String[] args) {
         mainMethod(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-        mainMethod(new int[][]{{5,1,9,11},{2,4,8,10},{13,3,6,7}, {15,14,12,16}});
-        mainMethod(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}});
-}
+        mainMethod(new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}});
+        mainMethod(new int[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}});
+    }
 
     private static void mainMethod(int[][] ints) {
         display(ints);
