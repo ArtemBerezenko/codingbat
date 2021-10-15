bcf3fb027f437924894e8c2c109e2011427670fc
ArtemBerezenko <a.a.berezenko@gmail.com>
2021-10-15 07:09:38 +0300

    leetcode::35: Search Insert Position

--- src/com/company/Leetcode/SearchInsertPosition.java
+++ src/com/company/Leetcode/SearchInsertPosition.java
@@ -9,10 +9,27 @@ public class SearchInsertPosition {
         return i;
     }
 
+    public static int binarySearchInsert(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        while (l <= h) {
+            int mid = l + ((h - l) / 2);
+            if (target > nums[mid]) {
+                l = mid + 1;
+            } else if (target < nums[mid]){
+                h = mid - 1;
+            } else return mid;
+
+        }
+        return l;
+    }
+
     public static void main(String[] args) {
-        System.out.println(searchInsert(new int[]{1,3,5,6}, 5));
-        System.out.println(searchInsert(new int[]{1,3,5,6}, 2));
-        System.out.println(searchInsert(new int[]{1,3,5,6}, 7));
-        System.out.println(searchInsert(new int[]{1,3,5,6}, 0));
+        System.out.println(binarySearchInsert(new int[]{1,3,5,6}, 5));
+        System.out.println(binarySearchInsert(new int[]{1,3,5,6}, 2));
+        System.out.println(binarySearchInsert(new int[]{1,3,5,6}, 7));
+        System.out.println(binarySearchInsert(new int[]{1,3,5,6}, 0));
+        System.out.println(binarySearchInsert(new int[]{1,3}, 2));
+
     }
 }
