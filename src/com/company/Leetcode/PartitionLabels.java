5619efc3f9460931b69dd32ee616ea727ac939d1
ArtemBerezenko <a.a.berezenko@gmail.com>
2021-12-18 20:23:08 +0300

    leetcode::763::Partition Labels

---
+++ src/com/company/Leetcode/PartitionLabels.java
@@ -0,0 +1,40 @@
+package com.company.Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels {
+
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        while ((i = findNextPartitionLength(s = s.substring(i))) != -1) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    private static int findNextPartitionLength(String s) {
+        if (s.isEmpty()) return -1;
+        int l = 0;
+        int h = s.length() - 1;
+        int max = 0;
+
+        while (l <= max) {
+            while (s.charAt(h) != s.charAt(l)) {
+                h--;
+            }
+            if (h > max) {
+                max = h;
+            }
+            l++;
+            h = s.length() - 1;
+        }
+        return max + 1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
+    }
+}
