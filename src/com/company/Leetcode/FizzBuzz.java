f39daff26a58071acf44fe0c193f9a77ef5f1e52
ArtemBerezenko <a.a.berezenko@gmail.com>
2022-08-21 00:04:31 +0300

    leetcode::412::Fizz Buzz

---
+++ src/com/company/Leetcode/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.company.Leetcode;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+
+    public static List<String> fizzBuzz(int n) {
+        return IntStream.rangeClosed(1, n)
+                .mapToObj(FizzBuzz::fizzBuzzz)
+                .collect(Collectors.toList());
+    }
+
+    private static String fizzBuzzz(Integer i) {
+        return switch (i) {
+            case Integer j && j % 3 == 0 && j % 5 == 0 -> "FizzBuzz";
+            case Integer j && j % 3 == 0 -> "Fizz";
+            case Integer j && j % 5 == 0 -> "Buzz";
+            default -> i.toString();
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(5));
+    }
+
+}
