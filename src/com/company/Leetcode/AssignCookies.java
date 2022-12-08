721dc1f9749522b982c8967da214e723b0692f40
ArtemBerezenko <a.a.berezenko@gmail.com>
2022-12-08 22:52:05 +0400

    LeetCode::455::Assign Cookies

---
+++ src/com/company/Leetcode/AssignCookies.java
@@ -0,0 +1,25 @@
+package com.company.Leetcode;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int l = 0;
+
+        while (l < s.length && i < g.length) {
+            if (s[l] >= g[i]) {
+                i++;
+            }
+            l++;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findContentChildren(new int[]{1,2}, new int[]{1,2,3}));
+    }
+}
