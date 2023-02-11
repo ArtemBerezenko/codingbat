48f48251666d45cb209dc3eef7f1cba0640ff65a
ArtemBerezenko <a.a.berezenko@gmail.com>
2023-02-11 17:55:13 +0400

    LeetCode::1663::Smallest String With A Given Numeric Value

---
+++ src/com/company/Leetcode/SmallestStringWithAGivenNumericValue.java
@@ -0,0 +1,37 @@
+package com.company.Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SmallestStringWithAGivenNumericValue {
+    public static String getSmallestString(int n, int k) {
+        String[] alphabet = new String[26];
+        int i = 0;
+        for (char ch = 'a'; ch <= 'z'; ++ch) {
+            alphabet[i++] = String.valueOf(ch);
+        }
+
+        List<String> resultList = new ArrayList<>();
+
+        k -= n;
+        i = 0;
+        while (k > 0) {
+            int index = Math.min(k, 25);
+            resultList.add(alphabet[index]);
+            k -= index;
+            i++;
+        }
+
+        while (n - i > 0) {
+            resultList.add(alphabet[0]);
+            i++;
+        }
+
+        return resultList.stream().sorted().collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSmallestString(5, 73));
+    }
+}
