049f3ea16d1d21d9583e3fc5a936af70d5a088de
ArtemBerezenko <a.a.berezenko@gmail.com>
2023-04-18 11:25:58 +0400

    LeetCode::31::Next Permutation

---
+++ src/com/company/leetcode/NextPermutation.java
@@ -0,0 +1,42 @@
+package com.company.leetcode;
+
+public class NextPermutation {
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3};
+        nextPermutation(ints);
+        System.out.print(ints);
+    }
+}
