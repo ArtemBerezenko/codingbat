bd7163709d3eee1b80ef9683e96445ea55023bd5
ArtemBerezenko <a.a.berezenko@gmail.com>
2023-12-23 23:55:15 +0100

    leetcode :: 1496. Path Crossing

---
+++ src/com/company/leetcode/PathCrossing.java
@@ -0,0 +1,44 @@
+package com.company.leetcode;
+
+import javafx.util.Pair;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PathCrossing {
+    
+    public static boolean isPathCrossing(String path) {
+        Map<String, Pair<Integer, Integer>> map = Map.of(
+                "N", new Pair<>(1, 0),
+                "S", new Pair<>(-1, 0),
+                "E", new Pair<>(0, 1),
+                "W", new Pair<>(0, -1)
+        );
+
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+        visited.add(new Pair<>(0, 0));
+        int x = 0;
+        int y = 0;
+        
+        for (Character c : path.toCharArray()) {
+            String s = String.valueOf(c);
+            Pair<Integer, Integer> pair = map.get(s);
+            x += pair.getKey();
+            y += pair.getValue();
+            Pair<Integer, Integer> visit = new Pair<>(x, y);
+
+            if (visited.contains(visit)) {
+                return true;
+            } 
+            
+            visited.add(visit);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPathCrossing("NNSWWEWSSESSWENNW"));
+    }
+}
