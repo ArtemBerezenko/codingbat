69efd379d58315adf03d3e634fd48459b866d440
ArtemBerezenko <a.a.berezenko@gmail.com>
2024-03-08 00:15:43 +0100

    leetcode :: 876. Middle of the Linked List

---
+++ src/com/company/leetcode/MiddleOfTheLinkedListII.java
@@ -0,0 +1,37 @@
+package com.company.leetcode;
+
+public class MiddleOfTheLinkedListII {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(middleNode(
+                new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))));
+    }
+}
