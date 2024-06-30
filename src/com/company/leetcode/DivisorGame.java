2d767763d70dcad02f17b29693e978d2e8d1893c
ArtemBerezenko <a.a.berezenko@gmail.com>
2024-07-01 00:42:28 +0200

    leetcode :: 1025. Divisor Game

---
+++ src/com/company/leetcode/DivisorGame.java
@@ -0,0 +1,11 @@
+package com.company.leetcode;
+
+public class DivisorGame {
+    public static boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divisorGame(5));
+    }
+}
