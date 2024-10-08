e60f727f3cbce25101b339efd23f0d28583eb898
ArtemBerezenko <a.a.berezenko@gmail.com>
2024-10-09 01:13:18 +0200

    leetcode :: 1. Two Sum

--- src/com/company/leetcode/TwoSum.java
+++ src/com/company/leetcode/TwoSum.java
@@ -1,8 +1,25 @@
 package com.company.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
+
+    public static int[] twoSumII(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) {
+                return new int[] {i, map.get(target - nums[i])};
+            }
+        }
+        return new int[0];
+    }
 
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -16,9 +33,9 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        int[] array = {2, 7, 11, 15};
-        int k = 9;
-        int[] result = twoSum(array, k);
+        int[] array = {3,2,4};
+        int k = 6;
+        int[] result = twoSumII(array, k);
         System.out.println(Arrays.toString(result));
     }
 }
