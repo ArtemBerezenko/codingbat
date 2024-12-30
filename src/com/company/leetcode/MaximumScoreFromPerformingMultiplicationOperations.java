b0c1844fb8f8f9b42db3a2fa36f444b4c637a4bd
ArtemBerezenko <a.a.berezenko@gmail.com>
2024-12-30 17:13:35 +0100

    leetcode :: 1770. Maximum Score from Performing Multiplication Operations Bottom Up

--- src/com/company/leetcode/MaximumScoreFromPerformingMultiplicationOperations.java
+++ src/com/company/leetcode/MaximumScoreFromPerformingMultiplicationOperations.java
@@ -26,7 +26,23 @@ public class MaximumScoreFromPerformingMultiplicationOperations {
         return memo[i][left];
     }
 
+    public static int maximumScore_Bottom_Up(int[] nums, int[] multipliers) {
+        int n = nums.length;
+        int m = multipliers.length;
+        int[][] dp = new int[m + 1][m + 1];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int right = n - 1 - (i - left);
+                dp[i][left] = Math.max(multipliers[i] * nums[left] + dp[i + 1][left + 1],
+                        multipliers[i] * nums[right] + dp[i + 1][left]);
+            }
+        }
+
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
-        System.out.println(maximumScore(new int[] {1,2,3}, new int[] {3,2,1}));
+        System.out.println(maximumScore_Bottom_Up(new int[] {1,2,3}, new int[] {3,2,1}));
     }
 }
