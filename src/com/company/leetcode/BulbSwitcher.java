645afc778a0315f205d8344d4efbb46803825fd9
ArtemBerezenko <a.a.berezenko@gmail.com>
2025-07-01 00:04:27 +0200

    leetcode :: 319. Bulb Switcher

---
+++ src/com/company/leetcode/BulbSwitcher.java
@@ -0,0 +1,11 @@
+package com.company.leetcode;
+
+public class BulbSwitcher {
+    public static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bulbSwitch(10));
+    }
+}
