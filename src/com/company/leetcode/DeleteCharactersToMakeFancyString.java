6749f28fb86ac3aac4abed6fd569d96c0a5c1694
ArtemBerezenko <a.a.berezenko@gmail.com>
2025-07-21 22:24:31 +0200

    leetcode :: 1957. Delete Characters to Make Fancy String

---
+++ src/com/company/leetcode/DeleteCharactersToMakeFancyString.java
@@ -0,0 +1,26 @@
+package com.company.leetcode;
+
+public class DeleteCharactersToMakeFancyString {
+    public static String makeFancyString(String s) {
+        var sb = new StringBuilder();
+
+        if (s.length() < 3) {
+            return s;
+        }
+
+        sb.append(s.charAt(0));
+        sb.append(s.charAt(1));
+
+        for (int i = 2; i < s.length(); i++) {
+            if (s.charAt(i - 2) == s.charAt(i - 1) && s.charAt(i - 1) == s.charAt(i)) {
+                continue;
+            }
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeFancyString("aaabaaaa"));
+    }
+}
