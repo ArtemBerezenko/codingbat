bfab1a681a853f018590b24e6bed926d2b05d0b0
ArtemBerezenko <a.a.berezenko@gmail.com>
2026-02-09 00:23:32 +0100

    leetcode :: 110. Balanced Binary Tree

--- src/com/company/leetcode/BalancedBinaryTree.java
+++ src/com/company/leetcode/BalancedBinaryTree.java
@@ -37,18 +37,49 @@ public class BalancedBinaryTree {
         return Math.max(leftCount, rightCount);
     }
 
+    public static boolean isBalanced2(TreeNode root) {
+        return count(root) != -1;
+    }
+
+    private static int count(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int l = count(node.left);
+        int r = count(node.right);
+        if (l == -1 || r == -1) {
+            return -1;
+        }
+        if (Math.abs(l - r) > 1) {
+            return -1;
+        }
+        return Math.max(l, r) + 1;
+    }
 
     public static void main(String[] args) {
 //        TreeNode rootLeft = new TreeNode(2, new TreeNode(3), new TreeNode(4));
 //        TreeNode rootRight = new TreeNode(2, new TreeNode(4), new TreeNode(3));
 //        TreeNode root = new TreeNode(1, rootLeft, rootRight);
 
-        TreeNode root = new TreeNode(3,
-                new TreeNode(9, new TreeNode(15), new TreeNode(7)),
-                new TreeNode(20)
-        );
+//        TreeNode root = new TreeNode(3,
+//                new TreeNode(9, new TreeNode(15), new TreeNode(7)),
+//                new TreeNode(20)
+//        );
 
-        System.out.println(isBalanced(root));
+        TreeNode root =
+                new TreeNode(1,
+                        new TreeNode(2,
+                                new TreeNode(3,
+                                        new TreeNode(4),
+                                        new TreeNode(4)
+                                ),
+                                new TreeNode(3)
+                        ),
+                        new TreeNode(2)
+                );
+
+
+        System.out.println(isBalanced2(root));
 
     }
 }
