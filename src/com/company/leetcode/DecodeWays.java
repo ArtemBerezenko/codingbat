eff4ed5cf28a7bd81ff8aecbff47874d2fd23da1
ArtemBerezenko <a.a.berezenko@gmail.com>
2026-04-10 00:27:53 +0200

    leetcode :: 91. Decode Ways

---
+++ src/com/company/leetcode/DecodeWays.java
@@ -0,0 +1,26 @@
+package com.company.leetcode;
+
+public class DecodeWays {
+    public static int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i < dp.length; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            if (i >= 2) {
+                int digit = Integer.parseInt(String.valueOf(s.charAt(i - 2)).concat(String.valueOf(s.charAt(i - 1))));
+                if (digit >= 10 && digit <= 26) {
+                    dp[i] += dp[i - 2];
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numDecodings("226"));
+    }
+}
